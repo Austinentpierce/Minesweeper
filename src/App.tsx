bab46bd3fbc7cb48a76eb949a2b384ed20d00069
Austin Pierce <Austinentpierce@gmail.com>
2021-09-14 08:34:56 -0400

    HTML got deleted accidently before commit redoing now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,69 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 
 export function App() {
+  const [game, setGame] = useState({
+    id: undefined,
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+    state: undefined,
+    mines: undefined,
+  })
+  async function newGame () {
+    const gameOptions = { difficulty: 0 }
+
+    const url = 'https://minesweeper-api.herokuapp.com/games'
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json'},
+      body: JSON.stringify(gameOptions),
+    }
+
+    const response = await fetch(url, fetchOptions)
+
+    console.log(response)
+
+    if (response.ok) {
+      const newGameStateJSON = await response.json()
+
+      setGame(newGameStateJSON)
+    }
+  }
+
+  async function handleClickCell(row: number, col: number) {
+    const checkOptions = {
+      id: game.id, 
+      row,
+      col,
+    }
+
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/JSON'},
+      body: JSON.stringify(checkOptions),
+    }
+
+    const response = await fetch(url, fetchOptions)
+
+    if (response.ok) {
+      const newGameStateJSON = await response.json()
+
+      setGame(newGameStateJSON)
+    }
+  }
+  async function handleRightClickCell
+    
+  }
+    
+  }
   return <div>Hello, World</div>
 }
